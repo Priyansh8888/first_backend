651c03747ddd60e757b0e32e9a494eed1ac36022
Priyansh Saxena <priyansh.saxena2021@vitbhopal.ac.in>
2024-07-07 20:54:10 +0530

    util codes

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { json } from "express"
+import express from express
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}))
+ app.use(express.json({limit:"16kb"}))
+ app.use(express.urlencoded({limit:"16kb"}))
+ app.use(express.static("public"))
+ app.use(cookieParser())
+
+export {app}
