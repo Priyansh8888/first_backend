753af83443b3e85d84e296188f1e341d340b3fdd
Priyansh Saxena <priyansh.saxena2021@vitbhopal.ac.in>
2024-07-28 01:56:22 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-import { json } from "express"
-import express from express
+
+import express from "express";
+import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app=express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    Credential: true
+    Credentials: true
 }))
  app.use(express.json({limit:"16kb"}))
  app.use(express.urlencoded({limit:"16kb"}))
  app.use(express.static("public"))
  app.use(cookieParser())
+
+ //import routes
+ import userRouter from './routes/user.routes.js'
+ app.use("/api/v1/users", userRouter)
 
 export {app}
